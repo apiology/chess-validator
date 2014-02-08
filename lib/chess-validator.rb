a0e2755252f46f86c55b59160bdd5944ebfe9630
Vince Broz <vince@broz.cc>
2014-02-08 14:52:37 -0500

    Add additional test cases.  Create separate Board object

--- lib/chess-validator.rb
+++ lib/chess-validator.rb
@@ -1,13 +1,63 @@
 module ChessValidator
+
+  class Board
+    def piece_type(from)
+      if [2, 7].include? rank(from)
+        :pawn
+      elsif [8].include? rank(from)
+        :rook
+      else
+        :invalid
+      end
+    end
+
+    private
+
+    def rank(position)
+      position[1].to_i
+    end
+
+    def file(position)
+      position[0].ord - 'a'.ord + 1
+    end
+  end
+
   class MoveEvaluator
+    def initialize(board)
+      @board = board
+    end
+
     # Ranks are numbers are rows, Files are letters are columns
     def evaluate(from, to)
-      distance = to[1].to_i - from[1].to_i
-      if distance != 3
-        true
-      else
+      type = @board.piece_type(from)
+      if type == :pawn
+        vertical_delta(from, to) != 3 &&
+          horizontal_delta(from, to) == 0
+      elsif type == :rook
+        vertical_delta(from, to) == 2
+      elsif type == :invalid
         false
+      else
+        fail
       end
+    end
+
+    private
+
+    def horizontal_delta(from, to)
+      (file(to) - file(from)).abs
+    end
+
+    def vertical_delta(from, to)
+      (rank(to) - rank(from)).abs
+    end
+
+    def rank(position)
+      position[1].to_i
+    end
+
+    def file(position)
+      position[0].ord - 'a'.ord + 1
     end
   end
 end
--- spec/chess-validator/move_evaluator_spec.rb
+++ spec/chess-validator/move_evaluator_spec.rb
@@ -2,26 +2,76 @@ require 'spec_helper'
 require 'chess-validator'
 
 describe ChessValidator::MoveEvaluator do
-  subject(:evaluator) { ChessValidator::MoveEvaluator.new }
+  def good(piece_type, from, to)
+    try_move(piece_type, from, to, true)
+  end
 
-  context "opening board" do
-    { black: 'a', white: 'b'}.each do |color, file|
-      context "#{color} pawn" do
-        it "advances 1x" do
-          expect(evaluator.evaluate('a2', 'a3')).to eq true
-        end
+  def bad(piece_type, from, to)
+    try_move(piece_type, from, to, false)
+  end
 
-        it "advances 2x" do
-          expect(evaluator.evaluate('a2', 'a4')).to eq true
-        end
+  def try_move(piece_type, from, to, ret)
+    allow(opening_board).to receive(:piece_type).and_return(piece_type)
+    expect(evaluator.evaluate(from, to)).to eq ret
+  end
 
-        it "advances 3x" do
-          expect(evaluator.evaluate('a2', 'a5')).to eq false
-        end
+  context "In the opening board" do
+    subject(:opening_board) { double("opening board") }
+    subject(:evaluator) { ChessValidator::MoveEvaluator.new(opening_board) }
 
-        it "advances 3x" do
-          expect(evaluator.evaluate('a2', 'a5')).to eq false
-        end
+    context "the white pawn" do
+      it "advances one or two moves, but not three" do
+        good(:pawn, 'a2', 'a3')
+        good(:pawn, 'a2', 'a4')
+        bad(:pawn, 'a2', 'a5')
+        good(:pawn, 'e2', 'e3')
+      end
+    end
+
+    context "the black pawn" do
+      it "advances one or two moves, but not three" do
+        good(:pawn, 'a7', 'a6')
+        good(:pawn, 'a7', 'a5')
+        bad(:pawn, 'a7', 'a4')
+      end
+
+      it "tries a sneaky diagonal move" do
+        bad(:pawn, 'a7', 'b6')
+      end
+    end
+
+    context "the black rook" do
+      it "hooks down and to the left" do
+        good(:rook, 'b8', 'a6')
+      end
+      it "hooks down and to the right" do
+        good(:rook, 'b8', 'c6')
+      end
+      it "hooks too far to the right" do
+        bad(:rook, 'b8', 'd7')
+      end
+    end
+
+    context "invalid piece" do
+      it "tries any move" do
+        bad(:invalid, 'e3', 'e2')
+      end
+    end
+  end
+
+  context "In a more complex board" do
+    subject(:opening_board) { IO.read('spec/samples/complex_board.txt')}
+    subject(:evaluator) { ChessValidator::MoveEvaluator.new(opening_board) }
+    context "white pawn" do
+      it "tries to jump to hyperspace" do
+        bad(:pawn, 'f2', 'b7')
+      end
+
+
+    end
+    context "piece not on the board" do
+      it "tries to do much of anything" do
+        bad(:invalid, 'g7', 'g2')
       end
     end
   end
