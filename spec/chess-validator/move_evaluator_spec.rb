6b3c7811148d2b35d5df2fe50fc6be11446a76b2
Vince Broz <vince@broz.cc>
2014-02-08 17:04:44 -0500

    Add more specs from wikipedia

--- spec/chess-validator/move_evaluator_spec.rb
+++ spec/chess-validator/move_evaluator_spec.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 require 'chess-validator'
 
@@ -11,14 +12,18 @@ describe ChessValidator::MoveEvaluator do
   end
 
   def try_move(piece_type, from, to, ret)
-    allow(opening_board).to receive(:piece_type).and_return(piece_type)
+    allow(board).to receive(:piece_type).and_return(piece_type)
     expect(evaluator.evaluate(ChessValidator::Position.new(from),
                               ChessValidator::Position.new(to))).to eq ret
   end
 
+  it "can't stand still", wip: true
+
+  it "tries to capture own piece", wip: true
+
   context "In the opening board" do
-    subject(:opening_board) { double("opening board") }
-    subject(:evaluator) { ChessValidator::MoveEvaluator.new(opening_board) }
+    subject(:board) { double("opening board") }
+    subject(:evaluator) { ChessValidator::MoveEvaluator.new(board) }
 
     context "the white pawn" do
       it "advances one or two moves, but not three" do
@@ -61,8 +66,8 @@ describe ChessValidator::MoveEvaluator do
   end
 
   context "In a more complex board" do
-    subject(:opening_board) { IO.read('spec/samples/complex_board.txt')}
-    subject(:evaluator) { ChessValidator::MoveEvaluator.new(opening_board) }
+    subject(:board) { double('complex board') }
+    subject(:evaluator) { ChessValidator::MoveEvaluator.new(board) }
     context "white pawn" do
       it "tries to jump to hyperspace" do
         bad(:pawn, 'f2', 'b7')
@@ -74,5 +79,117 @@ describe ChessValidator::MoveEvaluator do
         bad(:invalid, 'g7', 'g2')
       end
     end
+
+    # http://en.wikipedia.org/wiki/Rules_of_chess#Basic_moves
+
+    # The king moves exactly one square horizontally, vertically, or
+    # diagonally. A special move with the king known as castling is
+    # allowed only once per player, per game (see below).
+    context "the king" do
+      context "moves exactly one square" do
+        it "horizontally" do
+          good(:king, 'a8', 'b8')
+        end
+
+        it "vertically" do
+          good(:king, 'a8', 'a7')
+        end
+
+        it "diagonally" do
+          good(:king, 'a8', 'b7')
+        end
+      end
+
+      it "tries to go somewhere bizarre" do
+        bad(:king, 'a8', 'g5')
+      end
+
+      # A special move with the king known as castling is allowed only
+      # once per player, per game (see below).
+      it "tries to castle", wip: true
+    end
+
+    # A rook moves any number of vacant squares in a horizontal or
+    # vertical direction. It also is moved when castling.
+    context "the rook" do
+      context "moves any number of vacant squares" do
+        it "in a horizontal direction" do
+          good(:rook, 'a3', 'b3')
+        end
+
+        it "in a vertical direction" do
+          good(:rook, 'a3', 'a4')
+          good(:rook, 'a3', 'a2')
+          good(:rook, 'a3', 'a1')
+        end
+
+        it "but not in a diagonal direction" do
+          bad(:rook, 'a3', 'b4')
+        end
+      end
+
+      context "it cannot move any other way", wip: true
+
+      context "it cannot skip over occupied squares", wip: true
+
+      it "It also is moved when castling.", wip: true
+    end
+
+
+    # A bishop moves any number of vacant squares in any diagonal direction.
+    context "the bishop" do
+      it "moves any number of vacant squares" do
+        good(:bishop, 'f8', 'h6')
+        good(:bishop, 'f8', 'h6')
+        good(:bishop, 'f8', 'e7')
+        good(:bishop, 'f8', 'd6')
+      end
+
+      it "does not move in any other direction" do
+        bad(:bishop, 'f8', 'f7')
+        bad(:bishop, 'f8', 'c1')
+      end
+
+      context "it cannot skip over occupied squares", wip: true
+    end
+
+    # The queen moves any number of vacant squares in a horizontal,
+    # vertical, or diagonal direction.
+    context "the queen" do
+      context "moves any number of vacant squares in a " do
+        it "vertical direction", wip: true
+        it "horizontal direction", wip: true
+        it "diagonal direction", wip: true
+        it "does not move in any other direction", wip: true
+      end
+    end
+
+    # A knight moves to the nearest square not on the same rank, file,
+    # or diagonal. (This can be thought of as moving two squares
+    # horizontally then one square vertically, or moving one square
+    # horizontally then two squares vertically—i.e. in an "L"
+    # pattern.) The knight is not blocked by other pieces: it jumps to
+    # the new location.
+    context "the knight", wip: true
+
+    # Pawns have the most complex rules of movement:
+
+    # A pawn moves straight forward one square, if that square is
+    # vacant.
+    #
+    # If it has not yet moved, a pawn also has the option of moving
+    # two squares straight forward, provided both squares are
+    # vacant.
+    #
+    # Pawns cannot move backwards.
+    #
+    # Pawns are the only pieces that capture differently from how they
+    # move. A pawn can capture an enemy piece on either of the two
+    # squares diagonally in front of the pawn (but cannot move to
+    # those squares if they are vacant).
+    #
+    # The pawn is also involved in the two special moves en passant
+    # and promotion (Schiller 2003:17–19).
+    context "the pawn", wip: true
   end
 end
