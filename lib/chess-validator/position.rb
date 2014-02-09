c797a065fa199aca371597a2368471dca7afe011
Vince Broz <vince@broz.cc>
2014-02-09 12:40:18 -0500

    Add documentation

--- lib/chess-validator/position.rb
+++ lib/chess-validator/position.rb
@@ -1,4 +1,5 @@
 module ChessValidator
+  # Holds rank and file of positions, both as 1-indexed numbers.
   class Position
     attr_reader :rank, :file
 
@@ -10,6 +11,7 @@ module ChessValidator
     def self.parse(str)
       @str = str
       rank = @str[1].to_i
+      # turn letters into numbers
       file = @str[0].ord - 'a'.ord + 1
       Position.new(rank, file)
     end
